9f46841da6e35aecc442cdfc7287122db9af3fd9
Taneli Saarenkunnas <tauno_97@hotmail.com>
2024-03-11 11:14:59 +0200

    Korjaus takaisin

--- webmob2024/src/index.js
+++ webmob2024/src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
