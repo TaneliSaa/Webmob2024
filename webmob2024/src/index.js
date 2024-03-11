d89cc486326965265fa52f4fd2466ebae2f2a904
Taneli Saarenkunnas <tauno_97@hotmail.com>
2024-03-11 17:16:17 +0200

    Korjaus

--- webmob2024/src/index.js
+++ webmob2024/src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route, HashRouter } from 'react-router-dom';
 import { Etusivu } from './Komponentit/Etusivu';
 import { SormustenRitarit } from './Komponentit/SormustenRitarit';
 import { KaksiTornia } from './Komponentit/KaksiTornia';
 import { KuninkaanPaluu } from './Komponentit/KuninkaanPaluu';
 import { Kartat } from './Komponentit/Kartat';
-
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
